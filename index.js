56cbbf96796143fca311d37e92d44e2c1b5a676d
qin2007 <qin@ractoc.com>
2025-06-23 20:04:46 +0200

    git add ,

---
+++ index.js
@@ -0,0 +1,36 @@
+// antHelpers
+/**
+ * transform '\r\n' and '\r' to '\n'
+ * @param string
+ */
+export function normalize_newlines(string) {
+    return String(string).replace(/\r\n/g, '\n').replace(/\r/g, '\n');
+}
+/**
+ * adds 4 spaces to all '\n'
+ * @param string
+ */
+export function indent_codeblock(string) {
+    return '    ' + normalize_newlines(string).replace(/\n/g, '\n    ');
+}
+/**
+ * encodes a json with an indent
+ * @param jsonicItem the item you want to jsonify
+ * @param indent a number corresponding to indent, use true for 2
+ * @param replacer JSON.stringify's replacer
+ */
+export function jsonEncode(jsonicItem, indent = false, replacer) {
+    if (indent === false)
+        return JSON.stringify(jsonicItem, replacer);
+    else
+        return JSON.stringify(jsonicItem, replacer, indent === true ? 2 : +indent);
+}
+/**
+ * like jsonEncode but also adds 4 spaces to it
+ * @param jsonicItem
+ * @param indent
+ * @param replacer
+ */
+export function jsonEncodeIndent(jsonicItem, indent = true, replacer) {
+    return indent_codeblock(jsonEncode(jsonicItem, indent, replacer));
+}
